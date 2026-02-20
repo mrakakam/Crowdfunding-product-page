81807af7f47f7323196c6f20a11d9c759be73c63
mrakakam <johnakakamjohn@gmail.com>
2026-02-20 15:40:16 -0800

    editing the structures

--- crowdfundingwebapp/src/components/modal.jsx
+++ crowdfundingwebapp/src/components/modal.jsx
@@ -22,6 +22,173 @@ export const Modal = () => {
 
                 </div>
 
+
+                <div>
+
+                    <label htmlFor="">
+
+                        <input type="radio" name="pledge" id="" />
+
+                        <div>
+
+                            <h3>Pledge with no reward</h3>
+
+                            <p> Choose to support us without a reward if you simply believe in our project. As a <br/> 
+                                backer, you will be signed up to receive product updates via email 
+                            </p>
+
+
+                        </div>
+                    </label>
+
+
+                    <label htmlFor="">
+
+
+                        <input type="radio" name="bamboo-stand" id="" />
+
+                        <div>
+
+                            <span>
+
+                                <h3>Bamboo Stand</h3>
+
+                                <p>Pledge $25 or more</p>
+
+                            </span>
+
+                            <span>
+
+                                <h3>101</h3>
+
+                                <p>left</p>
+
+                            </span>
+
+                            <p>
+                                You get an ergonomic stand made of natural bamboo. You've helped us launch <br/> 
+                                 our promotional campaign, and you'll be added to a special Backer member list.
+                            </p>
+
+
+                            <div>
+
+                                <p>Enter your pledge</p>
+
+                                <div>
+
+                                    <input type="button" value="Continue" placeholder='$25' />
+
+                                    <button>Continue</button>
+
+                                </div>
+
+                            </div>
+
+
+                        </div>
+
+
+                    </label>
+                    <label htmlFor="">
+
+
+                        <input type="radio" name="bamboo-stand" id="" />
+
+                        <div>
+
+                            <span>
+
+                                <h3>Bamboo Stand</h3>
+
+                                <p>Pledge $25 or more</p>
+
+                            </span>
+
+                            <span>
+
+                                <h3>101</h3>
+
+                                <p>left</p>
+
+                            </span>
+
+                            <p>
+                                You get an ergonomic stand made of natural bamboo. You've helped us launch <br/> 
+                                 our promotional campaign, and you'll be added to a special Backer member list.
+                            </p>
+
+
+                            <div>
+
+                                <p>Enter your pledge</p>
+
+                                <div>
+
+                                    <input type="button" value="Continue" placeholder='$25' />
+
+                                    <button>Continue</button>
+
+                                </div>
+
+                            </div>
+
+
+                        </div>
+
+
+                    </label>
+                    <label htmlFor="">
+
+
+                        <input type="radio" name="bamboo-stand" id="" />
+
+                        <div>
+
+                            <span>
+
+                                <h3>Bamboo Stand</h3>
+
+                                <p>Pledge $25 or more</p>
+
+                            </span>
+
+                            <span>
+
+                                <h3>101</h3>
+
+                                <p>left</p>
+
+                            </span>
+
+                            <p>
+                                You get an ergonomic stand made of natural bamboo. You've helped us launch <br/> 
+                                 our promotional campaign, and you'll be added to a special Backer member list.
+                            </p>
+
+
+                            <div>
+
+                                <p>Enter your pledge</p>
+
+                                <div>
+
+                                    <input type="button" value="Continue" placeholder='$25' />
+
+                                    <button>Continue</button>
+
+                                </div>
+
+                            </div>
+
+
+                        </div>
+
+
+                    </label>
+
+                </div>
+
             </section>
         
         </>
