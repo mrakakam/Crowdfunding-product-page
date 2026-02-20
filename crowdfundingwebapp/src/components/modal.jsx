1993b8bc87ab1c1627a59372d39f3f6f6a04031b
mrakakam <johnakakamjohn@gmail.com>
2026-02-20 15:04:43 -0800

    just wrote an function for modal.jsx

--- crowdfundingwebapp/src/components/modal.jsx
+++ crowdfundingwebapp/src/components/modal.jsx
@@ -1,1 +1,15 @@
+import checkimg from '../assets/images/icon-check.svg';
+import closeimg from "../assets/images/icon-close-menu.svg";
 
+export const Modal = () => {
+
+    return (
+        
+        
+        <>
+        
+        
+        
+        </>
+    )
+}
